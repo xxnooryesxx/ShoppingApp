2c41ffa45805f2ec98236060ce3eb3a5235a46b9
xxnooryesxx <xxnooryesxx@gmail.com>
2019-09-18 15:10:48 -0500

    first commit

---
+++ ShoppingApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShoppingApp
+//
+//  Created by 愤怒大葱鸭 on 9/18/19.
+//  Copyright © 2019 愤怒大葱鸭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShoppingApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ShoppingApp
+//
+//  Created by 愤怒大葱鸭 on 9/18/19.
+//  Copyright © 2019 愤怒大葱鸭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
