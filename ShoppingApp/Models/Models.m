e5896ec7e1ee819e3db54d85bd196ae612e42eae
Nai Ayobi <naiscalemail@gmail.com>
2019-09-19 14:37:36 -0500

    Added initial models

---
+++ ShoppingApp/Models/Models.m
@@ -0,0 +1,26 @@
+//
+//  Item.m
+//  ShoppingApp
+//
+//  Created by Nawid Ayobi on 9/19/19.
+//  Copyright © 2019 愤怒大葱鸭. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+struct Item {
+    NSString *name;
+    NSString *category;
+    NSString *subcategory;
+    double price;
+};
+
+struct User {
+    NSString *firstName;
+    NSString *lastName;
+    NSString *mobile;
+    NSString *email;
+    NSString *password;
+    NSString *sellerId;
+};
+
