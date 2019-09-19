49b5c785a1cb60fe02a3958558ba98bff9fbd2b0
Nai Ayobi <naiscalemail@gmail.com>
2019-09-19 14:23:31 -0500

    Created SignUpVCs

---
+++ ShoppingApp/Views/SignUpBuyerViewController.h
@@ -0,0 +1,17 @@
+//
+//  SignUpBuyerViewController.h
+//  ShoppingApp
+//
+//  Created by Nawid Ayobi on 9/19/19.
+//  Copyright © 2019 愤怒大葱鸭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SignUpBuyerViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ShoppingApp/Views/SignUpSellerViewController.h
@@ -0,0 +1,17 @@
+//
+//  SignUpSellerViewController.h
+//  ShoppingApp
+//
+//  Created by Nawid Ayobi on 9/19/19.
+//  Copyright © 2019 愤怒大葱鸭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SignUpSellerViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
