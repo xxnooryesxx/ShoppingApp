3ea62585072bfee7b6b7b7fe24eee3fd2eb5b0cf
Min <msk67@pitt.edu>
2019-09-22 16:37:26 -0500

    commit

--- ShoppingApp/View/ViewController.m
+++ ShoppingApp/View/ViewController.m
@@ -27,7 +27,9 @@
     // Make api call
     NSString *phone = _phone.text;
     NSString *pwd = _password.text;
+    
     NSString *strURL = [NSString stringWithFormat:@"http://rjtmobile.com/aamir/e-commerce/ios-app/shop_login.php?mobile=%@&password=%@", phone, pwd];
+    
     //    NSURL *url = [NSURL URLWithString:strURL];
     NSURL *url = [NSURL URLWithString:@"http://rjtmobile.com/aamir/e-commerce/ios-app/shop_login.php?mobile=55565454&password=7011"];
     NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
@@ -36,6 +38,7 @@
     NSURLSessionDataTask *dataTask = [[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
         NSLog(@"%@", data);
     }];
+    
     [dataTask resume];
 }
 
